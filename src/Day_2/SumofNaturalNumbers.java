4f5e655e60fd18693d67c617ce8b8fa3e02833b4
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 13:40:58 +0530

    [Vansh]Add.Sum of Natural NUmbers

--- src/Day_2/SumofNaturalNumbers.java
+++ src/Day_2/SumofNaturalNumbers.java
@@ -1,5 +1,17 @@
 package Day_2;
-
+import java.util.*;
 public class SumofNaturalNumbers {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        
+        if (n >= 0) {
+            int sum = n * (n + 1) / 2;
+            System.out.println("The sum of " + n + " natural numbers is " + sum);
+        } else {
+            System.out.println("The number " + n + " is not a natural number");
+        }
+    }
 
 }
