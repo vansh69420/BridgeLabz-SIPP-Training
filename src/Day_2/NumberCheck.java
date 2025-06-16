ab7794cae789b111cfa1e4d41e474a423d0c9cc7
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 15:04:07 +0530

    [Vansh]Add.Number Check

---
+++ src/Day_2/NumberCheck.java
@@ -0,0 +1,18 @@
+package Day_2;
+import java.util.*;
+public class NumberCheck {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+}
