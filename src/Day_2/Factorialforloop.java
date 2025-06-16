f6c13376133eacfc519a6672c353ead6f03a8316
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 13:50:00 +0530

    [Vansh]Fix.Factorial(for loop)

--- src/Day_2/Factorialforloop.java
+++ src/Day_2/Factorialforloop.java
@@ -1,6 +1,6 @@
 package Day_2;
 import java.util.*;
-public class Factorialforloop {
+public class FactorialForLoop {
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a positive integer: ");
