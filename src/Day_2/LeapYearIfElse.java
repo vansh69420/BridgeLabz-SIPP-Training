2fb292e0ed11bcbc4db8492ec0c74549f35cfe74
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 15:28:31 +0530

    [Vansh]Add.Leap Year (if else)

---
+++ src/Day_2/LeapYearIfElse.java
@@ -0,0 +1,28 @@
+package Day_2;
+import java.util.*;
+public class LeapYearIfElse {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year (>= 1582): ");
+        int year = scanner.nextInt();
+        
+        if (year < 1582) {
+            System.out.println("Year must be >= 1582.");
+            return;
+        }
+        
+        if (year % 4 != 0) {
+            System.out.println(year + " is not a Leap Year");
+        } else {
+            if (year % 100 != 0) {
+                System.out.println(year + " is a Leap Year");
+            } else {
+                if (year % 400 == 0) {
+                    System.out.println(year + " is a Leap Year");
+                } else {
+                    System.out.println(year + " is not a Leap Year");
+                }
+            }
+        }
+    }
+}
