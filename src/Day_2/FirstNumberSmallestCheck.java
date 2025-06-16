123445a95b13487ff3799d4c801133a4cbeb1f11
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 13:59:10 +0530

    [Vansh]Add.First Number Smallest Check

---
+++ src/Day_2/FirstNumberSmallestCheck.java
@@ -0,0 +1,16 @@
+package Day_2;
+import java.util.*;
+public class FirstNumberSmallestCheck {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+        
+        boolean isSmallest = num1 < num2 && num1 < num3;
+        System.out.println("Is the first number the smallest? " + isSmallest);
+    }
+}
