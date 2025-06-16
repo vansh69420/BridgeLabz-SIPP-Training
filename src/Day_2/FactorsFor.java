a25b26340a87aab9e048e7a98da8c76959c46387
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 15:21:48 +0530

    [Vansh]Add.Factors (for)

---
+++ src/Day_2/FactorsFor.java
@@ -0,0 +1,21 @@
+package Day_2;
+import java.util.*;
+public class FactorsFor {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+            return;
+        }
+        
+        System.out.println("Factors of " + number + ":");
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
