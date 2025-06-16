08b3ef3c5d399b0611a016ba01455b3a379c1333
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 14:01:55 +0530

    [Vansh]Add.Sum Until Zero

---
+++ src/Day_2/SumUntilZero.java
@@ -0,0 +1,19 @@
+package Day_2;
+import java.util.*;
+public class SumUntilZero {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double total = 0.0;
+        
+        System.out.println("Enter numbers (0 to stop):");
+        while (true) {
+            double num = scanner.nextDouble();
+            if (num == 0) {
+                break;
+            }
+            total += num;
+        }
+        
+        System.out.println("Total sum: " + total);
+    }
+}
