6a19f176ade649a586e77656cd04f3759d0d0c27
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 15:34:00 +0530

    [Vansh]Add.Leap Year Single Condition

---
+++ src/Day_2/LeapYearSingleCondition.java
@@ -0,0 +1,20 @@
+package Day_2;
+import java.util.*;
+public class LeapYearSingleCondition {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year (>= 1582): ");
+        int year = scanner.nextInt();
+        
+        if (year < 1582) {
+            System.out.println("Year must be >= 1582.");
+            return;
+        }
+        
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a Leap Year");
+        } else {
+            System.out.println(year + " is not a Leap Year");
+        }
+    }
+}
