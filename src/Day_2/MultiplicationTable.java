33133c49e77c94996d5db6d1f1977f73fdf3e52d
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 15:10:52 +0530

    [Vansh]Add.Multiplication Table

---
+++ src/Day_2/MultiplicationTable.java
@@ -0,0 +1,13 @@
+package Day_2;
+import java.util.*;
+public class MultiplicationTable {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
