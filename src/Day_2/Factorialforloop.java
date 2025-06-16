8a9de76a6dd555d4cb227ae03eeffc629420e920
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 13:47:55 +0530

    [Vansh]Add.Factorial with loop

---
+++ src/Day_2/Factorialforloop.java
@@ -0,0 +1,21 @@
+package Day_2;
+import java.util.*;
+public class Factorialforloop {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = scanner.nextInt();
+        
+        if (num < 0) {
+            System.out.println("Please enter a positive integer.");
+            return;
+        }
+        
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        
+        System.out.println("Factorial of " + num + " is " + factorial);
+    }
+}
