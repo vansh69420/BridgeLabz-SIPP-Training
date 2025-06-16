1da8f61a824e03622232befdbe6b69cc8eb8361a
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 13:46:10 +0530

    [Vansh]Add.Spring Season Check

---
+++ src/Day_2/SpringSeasonCheck.java
@@ -0,0 +1,19 @@
+package Day_2;
+import java.util.*;
+public class SpringSeasonCheck {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter month (1-12): ");
+        int month = scanner.nextInt();
+        System.out.print("Enter day (1-31): ");
+        int day = scanner.nextInt();
+        
+        if ((month == 3 && day >= 20) || 
+            (month > 3 && month < 6) || 
+            (month == 6 && day <= 20)) {
+            System.out.println("It's a Spring Season");
+        } else {
+            System.out.println("Not a Spring Season");
+        }
+    }
+}
