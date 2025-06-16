5f012121b6c23052a1c5ca37b66b4283a3a54803
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-16 15:30:58 +0530

    [Vansh]Add.Abundant Number

---
+++ src/Day_2/AbundantNumber.java
@@ -0,0 +1,27 @@
+package Day_2;
+import java.util.*;
+public class AbundantNumber {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+            return;
+        }
+        
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        
+        if (sum > number) {
+            System.out.println(number + " is an Abundant Number");
+        } else {
+            System.out.println(number + " is not an Abundant Number");
+        }
+    }
+}
