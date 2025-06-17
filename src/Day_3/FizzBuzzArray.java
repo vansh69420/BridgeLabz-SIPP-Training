43e297a187c23dad1095edac58a144d2064cb19f
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-17 12:09:07 +0530

    [Vansh]Add.FizzBuzz Array

---
+++ src/Day_3/FizzBuzzArray.java
@@ -0,0 +1,32 @@
+package Day_3;
+import java.util.*;
+public class FizzBuzzArray {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+        
+        if (n <= 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        String[] results = new String[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            if (i % 15 == 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+    }
+}
