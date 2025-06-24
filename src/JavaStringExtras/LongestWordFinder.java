f259cedc5e1a319d37154d095c3a64d236ff1a77
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-24 10:20:13 +0530

    [Vansh]Add.Longest Word Finder

---
+++ src/JavaStringExtras/LongestWordFinder.java
@@ -0,0 +1,18 @@
+package JavaStringExtras;
+import java.util.*;
+public class LongestWordFinder {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        
+        String[] words = sentence.split(" ");
+        String longestWord = "";
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        System.out.println("Longest word: " + longestWord);
+    }
+}
