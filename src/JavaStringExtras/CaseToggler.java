a0d29fd816047c3f6e5ca05e13c3e609145accdc
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-06-24 10:28:56 +0530

    [Vansh]Add.Case Toggler

---
+++ src/JavaStringExtras/CaseToggler.java
@@ -0,0 +1,20 @@
+package JavaStringExtras;
+import java.util.*;
+public class CaseToggler {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        
+        StringBuilder toggled = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                toggled.append(Character.toLowerCase(ch));
+            } else {
+                toggled.append(Character.toUpperCase(ch));
+            }
+        }
+        System.out.println("Toggled case: " + toggled);
+    }
+}
