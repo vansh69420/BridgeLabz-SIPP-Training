c684e5a79d1edc9e89af430673b65030af3a9f98
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-07-02 13:21:27 +0530

    [Vansh]Add.Item.java file

---
+++ src/Day1_OOPs/Item.java
@@ -0,0 +1,23 @@
+package Day1_OOPs;
+
+public class Item {
+    private int itemCode;
+    private String itemName;
+    private double price;
+
+    public Item(int itemCode, String itemName, double price) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Item Code: " + itemCode);
+        System.out.println("Item Name: " + itemName);
+        System.out.println("Price: " + price);
+    }
+
+    public double calculateTotalCost(int quantity) {
+        return price * quantity;
+    }
+}
