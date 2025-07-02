c92d7d34a9e6c17c0d95c3f0b7f1b5e7767272a6
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-07-02 13:22:16 +0530

    [Vansh]Add.MobilePhone.java file

---
+++ src/Day1_OOPs/MobilePhone.java
@@ -0,0 +1,19 @@
+package Day1_OOPs;
+
+public class MobilePhone {
+    private String brand;
+    private String model;
+    private double price;
+
+    public MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price);
+    }
+}
