4a7260b5f3a77b724433e320ee8d3809dad49553
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-07-02 13:18:29 +0530

    [Vansh]Add.Day 1 OOPs and Employee.java file

---
+++ src/Day1_OOPs/Employee.java
@@ -0,0 +1,19 @@
+package Day1_OOPs;
+
+public class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+}
