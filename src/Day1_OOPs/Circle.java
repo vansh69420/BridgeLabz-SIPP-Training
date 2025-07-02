3a20d35487cb5c8013c701fc32d4df92957b2921
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-07-02 13:20:10 +0530

    [Vansh]Add.Circle.java file

---
+++ src/Day1_OOPs/Circle.java
@@ -0,0 +1,23 @@
+package Day1_OOPs;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Circumference: " + calculateCircumference());
+    }
+}
