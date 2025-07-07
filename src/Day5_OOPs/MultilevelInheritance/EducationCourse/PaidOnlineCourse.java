447c3bc8da4b0f1f9f3b95ff7a90f0b76c2c1f85
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-07-07 10:09:10 +0530

    [Vansh]Add.Education Course Hierarchy

--- src/Day5_OOPs/MultilevelInheritance/EducationCourse/PaidOnlineCourse.java
+++ src/Day5_OOPs/MultilevelInheritance/EducationCourse/PaidOnlineCourse.java
@@ -1,12 +1,12 @@
 package Day5_OOPs.MultilevelInheritance.EducationCourse;
 
 public class PaidOnlineCourse extends OnlineCourse {
-    private double fee;
+    private double fees;
     private double discount;
 
     public PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
         super(courseName, duration, platform, isRecorded);
-        this.fee = fee;
+        this.fees = fees;
         this.discount = discount;
     }
 }
